55c797ea76a84f552184bd7ff8b5a0559ce40d2c
Worawat <worawat.wijarn@gmail.com>
2022-11-16 22:08:25 +0700

    Fix release 0.20.1

--- provider/resources.go
+++ provider/resources.go
@@ -65,7 +65,7 @@ func Provider() tfbridge.ProviderInfo {
 		//
 		// You may host a logo on a domain you control or add an SVG logo for your package
 		// in your repository and use the raw content URL for that file as your logo URL.
-		LogoURL: "",
+		LogoURL: "https://raw.githubusercontent.com/oun/pulumi-flux/main/flux.svg",
 		// PluginDownloadURL is an optional URL used to download the Provider
 		// for use in Pulumi programs
 		// e.g https://github.com/org/pulumi-provider-name/releases/
